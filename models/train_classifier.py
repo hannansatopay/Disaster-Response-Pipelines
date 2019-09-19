7c094fe3d052b0259378954993e83de028a31bd1
Hannan Satopay <sathannan@hotmail.com>
2019-09-19 17:45:49 +0530

    Update the Machine Learning Pipeline

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,109 @@
 import sys
-
+import pickle
+import re
+import nltk
+nltk.download(['punkt', 'wordnet'])
+nltk.download('stopwords')
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import pandas as pd
+from sqlalchemy import create_engine
+from nltk.corpus import stopwords
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 def load_data(database_filepath):
-    pass
+    """
+    Loads data from database
+    Args:
+        database_filepath: path to database
+    Returns:
+        (DataFrame) X: Message data (features)
+        (DataFrame) y: Categories (target)
+        category_names: Labels for categories
+    """
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_query('select * from cleanData', engine)
+
+    X = df['message']
+    y = df.iloc[:,4:]
+    category_names = y.columns
+    return X, y, category_names
 
 
 def tokenize(text):
-    pass
+    """
+    Tokenizes a given text.
+    Args:
+        text: text string
+    Returns:
+        list of cleaned & tokenzied text
+    """
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    """
+    Builds classification model
+    Returns:
+        Results of GridSearchCV
+    """
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+
+    parameters = {
+        'vect__ngram_range': ((1, 1), (1, 2)),
+        'clf__estimator__n_estimators': [50, 100],
+        'clf__estimator__min_samples_split': [2, 4],
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters, verbose=2, n_jobs=4)
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """
+    Evaluate the model against a test dataset
+    Args:
+        model: Trained model
+        X_test: Test features
+        Y_test: Test labels
+        category_names: String array of category names
+    """
+    y_pred = model.predict(X_test)
+    for i in range(36):
+        print("Classification report for ", Y_test.columns[i], "\n", classification_report(Y_test.values[:,i],y_pred[:,i]))
+        print("Accuracy score for ", Y_test.columns[i], accuracy_score(Y_test.values[:,i],y_pred[:,i]))
 
 
 def save_model(model, model_filepath):
-    pass
+    """
+    Save the model to a Python pickle
+    Args:
+        model: Trained model
+        model_filepath: Path where to save the model
+    """
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
@@ -27,13 +112,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
